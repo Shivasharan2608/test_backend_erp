79951fdd668606920bbb1fb7a1c7b8f2ebf37512
Shivasharan2608 <shivasharan.nandoor@bitstek.io>
2026-08-28 13:48:16 +0530

    ERP backend

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ const app = express();
 
 await connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use((req, res, next) => {
